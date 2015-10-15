6135dd882fb4674e5fc84c7c39c0dca8b1d282c4
Sathya Anandan <sathya.anandan@proquest.com>
2015-10-15 16:02:27 -0700

    Initial Commit

---
+++ src/test/java/MyFirstJavaClass.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by SAnandan on 10/15/2015.
+ */
+public class MyFirstJavaClass {
+   @Test
+    public void myFirstGITProgram(){
+       System.out.print("This is my first GIT learning project");
+   }
+}
