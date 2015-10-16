c82c80d1e8a6740aa7101ba75fc1c67057e34469
Sathya Anandan <sathya.anandan@proquest.com>
2015-10-16 08:46:51 -0700

    Second Commit Testing

--- src/test/java/MyFirstJavaClass.java
+++ src/test/java/MyFirstJavaClass.java
@@ -7,5 +7,6 @@ public class MyFirstJavaClass {
    @Test
     public void myFirstGITProgram(){
        System.out.print("This is my first GIT learning project");
+       System.out.print("Testing Commiting");
    }
 }
